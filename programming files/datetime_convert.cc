5d6597c89f3a10c5a56421366d2647829d270384
astute404 <113051585+astute404@users.noreply.github.com>
2023-03-21 10:56:07 -0400

    Create datetime_convert.cc
    Added second : on line 15

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -9,10 +9,10 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30";
+    std::string date_str = "2022-03-17 10:45:30';
     std::tm date_obj = {};
     std::istringstream ss(date_str);
-    ss >> std:get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%d/%Y/%m %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str;
