ee13fc8756fac6080632b2fdf13eb7c3cad5b4e0
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2023-03-21 10:22:25 -0400

    Added comments

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,15 +1,21 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
+        // initialize variables
         int a = 0, b = 1;
         int sumEven = 0;
+      
         while (b < 4000000) {
+            // determine if b is even
             if (b % 2 == 0) {
+                // add b to sum if even
                 sumEven += b;
             }
+            // update values of a and b
             int temp = b;
             b = a + b;
             a = temp;
         }
+        // display sum of even values
         System.out.println(sumEven);
     }
 }
