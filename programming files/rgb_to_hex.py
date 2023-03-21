928c0b4e4309f4e7bfc487a2d81c1426525bacad
astute404 <113051585+astute404@users.noreply.github.com>
2023-03-21 10:29:01 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,9 +1,11 @@
+#Bugs introduced by: astute
+
 def rgb_to_hex(r, g, b):
     #Converting each color int value into hexadecimal
-    r = max(0, min(255, r))
+    r = max(17, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    b = max(0, min(100, b))
+    return '{:02X}{:03X}{:02X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
