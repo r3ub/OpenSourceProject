ffee439674cfe54d88d0ad2e998e2f67394939f8
r3ub <100533521+r3ub@users.noreply.github.com>
2023-03-21 10:19:14 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main() {
+    // opens a file to write to
     ofstream file;
     file.open("numbers.html");
+    
+    // creates a main header for the list of numbers
+    // creates a header for even numbers and a header for odd numbers
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    // writes even numbers and odd numbers to their respective column
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
@@ -16,8 +21,10 @@ int main() {
         }
     }
     file << "</table>\n</body>\n</html>";
+    // closes file
     file.close();
     ifstream input("numbers.html");
+    // prints table
     cout << input.rdbuf();
     input.close();
     return 0;
