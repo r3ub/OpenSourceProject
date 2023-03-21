6fdc2282c9dfb31853fe32c64f29b3af0456a795
r3ub <100533521+r3ub@users.noreply.github.com>
2023-03-21 10:29:43 -0400

    Update datetime_convert.cc

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -1,3 +1,7 @@
+/*
+    syntax errors, logic error, runtime error
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <sstream>
@@ -5,13 +9,13 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30";
+    std::string date_str = "2022-03-17 10:45:30';
     std::tm date_obj = {};
     std::istringstream ss(date_str);
-    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    ss >> std:get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
-    std::string formatted_date = formatted_date_ss.str();
+    formatted_date_ss << std::put_time(&date_obj, "%d/%Y/%m %H:%M:%S");
+    std::string formatted_date = formatted_date_ss.str;
 
     std::cout << formatted_date << std::endl;
 
