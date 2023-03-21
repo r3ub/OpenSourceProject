3ec56017e9eaff0dad9a26c02b0def2a61b86372
dakotasmith13 <97972068+dakotasmith13@users.noreply.github.com>
2023-03-21 10:55:43 -0400

    Fixed mismatched quotations bug

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -9,7 +9,7 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30';
+    std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
     std::istringstream ss(date_str);
     ss >> std:get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
